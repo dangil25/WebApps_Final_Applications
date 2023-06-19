19c1d576f38a025d44873db3272a29c3202c9511
gerinome <danielgtravels@gmail.com>
2023-06-19 19:06:41 -0400

    fixed sql

--- db/queries/crud/todo_order.sql
+++ db/queries/crud/todo_order.sql
@@ -1,7 +1,20 @@
-SET @priorities = ?;
-SET @dater = ?;
-SELECT applicationName, DATE_FORMAT(dueDate, "%m/%d/%y (%W)") AS dueDateFormatted, categoryName, essaySubmitted, recRequest, transcriptRequest, notes
-FROM applications
-JOIN categories on categories.categoryId = applications.categoryId
-WHERE (-1 = @priorities OR priority = @priorities) and ((0 = @dater and dueDate < CURRENT_TIMESTAMP) or (1 = @dater and dueDate > CURRENT_TIMESTAMP) or (2 = @dater)) and status = ? and applications.userId = ?
-ORDER BY priority desc, dueDate;
+
+SELECT
+    applicationName,
+    DATE_FORMAT(dueDate, "%m/%d/%y (%W)") AS dueDateFormatted,
+    categoryName,
+    essaySubmitted, recRequest, transcriptRequest,
+    notes,
+    @priorities = ?,
+    @dater = ?
+FROM applications 
+JOIN categories
+    ON applications.categoryId = categories.categoryId
+WHERE 
+    @priorities in (-1, priority)
+    AND ((0 = @dater and dueDate < CURRENT_TIMESTAMP) or (1 = @dater and dueDate > CURRENT_TIMESTAMP) or (2 = @dater))
+    AND status = ?
+    AND applications.userId = ?
+ORDER BY
+    priority desc, dueDate
+    
